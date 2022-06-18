184466399bf550d9192e69b2621fb4a691feed2b
M Irfan Fanani <36741093+sleepisfun17@users.noreply.github.com>
2022-06-18 23:00:33 +0700

    added tmp/

--- app.js
+++ app.js
@@ -3,31 +3,69 @@ const app = express();
 const port = process.env.PORT || 3000;
 const fs = require("fs");
 const recorder = require("node-record-lpcm16");
-const AudioRecorder = require("node-audiorecorder");
+
 // Imports the Google Cloud client library
 const speech = require("@google-cloud/speech");
-// import fetch from "node-fetch";
-// fungsi api key
-// Imports the Google Cloud client library.
-// const { Storage } = require("@google-cloud/storage");
+const { Storage } = require("@google-cloud/storage");
+const storage = new Storage();
+const myBucket = storage.bucket("my-bucket");
+const audioUpload = myBucket.file("record.wav");
+
+async function getTranscriptGoogleStorage() {
+  // Creates a client
+  const client = new speech.SpeechClient();
+
+  /**
+   * TODO(developer): Uncomment the following lines before running the sample.
+   */
+  const gcsUri = "gs://sistem-pencarian-345108.appspot.com/record.wav";
+  const encoding = "LINEAR16";
+  const sampleRateHertz = 16000;
+  const languageCode = "id-ID";
+
+  const config = {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  };
+
+  const audio = {
+    uri: gcsUri,
+  };
+
+  const request = {
+    config: config,
+    audio: audio,
+  };
+
+  // Detects speech in the audio file. This creates a recognition job that you
+  // can wait for now, or get its result later.
+  const [operation] = await client.longRunningRecognize(request);
+  // Get a Promise representation of the final result of the job
+  const [response] = await operation.promise();
+  const transcription = response.results.map((result) => result.alternatives[0].transcript).join("\n");
+  console.log(`Transcription: ${transcription}`);
+
+  this.transcription = transcription;
+}
 
 // fungsi rekaman audio
 function getRecord() {
-  const file = fs.createWriteStream("record.wav", { encoding: "binary" });
+  const file = fs.createWriteStream("tmp/record.wav", { encoding: "binary" });
 
   const recording = recorder.record();
   recording.stream().pipe(file);
 
   setTimeout(() => {
     file.end();
-  }, 5000);
+  }, 4000);
 }
 
 // fungsi transkrip audio to text
 async function getTranscript() {
   const client = new speech.SpeechClient();
 
-  const filename = "record.wav";
+  const filename = "tmp/record.wav";
   const encoding = "LINEAR16";
   const sampleRateHertz = 16000;
   const languageCode = "id-ID";
