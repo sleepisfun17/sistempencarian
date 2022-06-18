059901331856339a6a1cca4cd990978cc44e2efa
M Irfan Fanani <36741093+sleepisfun17@users.noreply.github.com>
2022-06-18 23:02:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,48 +6,6 @@ const recorder = require("node-record-lpcm16");
 
 // Imports the Google Cloud client library
 const speech = require("@google-cloud/speech");
-const { Storage } = require("@google-cloud/storage");
-const storage = new Storage();
-const myBucket = storage.bucket("my-bucket");
-const audioUpload = myBucket.file("record.wav");
-
-async function getTranscriptGoogleStorage() {
-  // Creates a client
-  const client = new speech.SpeechClient();
-
-  /**
-   * TODO(developer): Uncomment the following lines before running the sample.
-   */
-  const gcsUri = "gs://sistem-pencarian-345108.appspot.com/record.wav";
-  const encoding = "LINEAR16";
-  const sampleRateHertz = 16000;
-  const languageCode = "id-ID";
-
-  const config = {
-    encoding: encoding,
-    sampleRateHertz: sampleRateHertz,
-    languageCode: languageCode,
-  };
-
-  const audio = {
-    uri: gcsUri,
-  };
-
-  const request = {
-    config: config,
-    audio: audio,
-  };
-
-  // Detects speech in the audio file. This creates a recognition job that you
-  // can wait for now, or get its result later.
-  const [operation] = await client.longRunningRecognize(request);
-  // Get a Promise representation of the final result of the job
-  const [response] = await operation.promise();
-  const transcription = response.results.map((result) => result.alternatives[0].transcript).join("\n");
-  console.log(`Transcription: ${transcription}`);
-
-  this.transcription = transcription;
-}
 
 // fungsi rekaman audio
 function getRecord() {
